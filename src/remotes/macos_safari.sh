3f7c217fe50bdf645b1b08f64ce48f1100cbd52b
Arturo Nava <arturonavax@gmail.com>
2023-12-23 01:28:31 -0500

    macos

--- src/remotes/macos_safari.sh
+++ src/remotes/macos_safari.sh
@@ -41,6 +41,7 @@ defaults write com.apple.Safari FindOnPageMatchesWordStartsOnly -bool false
 
 # Enable the Develop menu and the Web Inspector in Safari
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari.SandboxBroker ShowDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
 
